908c4a00f99548c3f6fadaab0910c7850419f827
Enrico <142443117+enricoarj@users.noreply.github.com>
2024-10-22 17:29:43 -0300

    Update index.php
    Estado do código após aula 8

--- 3periodo/estudos_php/index.php
+++ 3periodo/estudos_php/index.php
@@ -26,38 +26,54 @@
         [
             'name' => 'Dracula',
             'author' => "Bram Stoker",
-            'purchase_url' => "https://example.com"
+            'purchase_url' => "https://example.com",
+            'release_date' =>  1992
         ],
         [
             'name' => "Carmilla, Karnstein's Vampire",
             'author' => "Sheridan Le Fanu",
-            'purchase_url' => "https://example.com"
+            'purchase_url' => "https://example.com",
+            'release_date' => 1872
         ],    
         [
             'name' => "The Fall Of The House of Usher",
             'author' =>"Edgar Allan Poe",
-            'purchase_url' => "https://example.com"
-
+            'purchase_url' => "https://example.com",
+            'release_date' => 1839
         ],
         [
             'name' => "Call of Cthulhu",
             'author' => "H.P. Lovecraft",
-            'purchase_url' => "https://example.com"
-
+            'purchase_url' => "https://example.com",
+            'release_date' => 1981
         ]
 
        ];
+    
+    function filter_by_author($books, $author){
+        $author = gets.chomp
+        $filtered_books = [];
+        foreach ($books as $book){
+            if ($book['author'] === $author ){
+                $filtered_books[] = $book;
+            }
+        }
+        return $filtered_books;
+    }
+
     ?>
+
     <ul>
         <?php 
-            foreach($books as $book) : ?>
-                <li>
-                    <a href ="<?= $book['purchase_url'] ?>">
-                        <?= $book['name'] ?>
-                    </a>
-                </li>
+            foreach(filter_by_author($books, $author) as $book) : ?>
+                    <li>
+                        <a href ="<?= $book['purchase_url'] ?>">
+                            <?= $book['name'] ?> (<?= $book['release_date']?>)
+                        </a>
+                    </li>
             <?php endforeach; ?>
     </ul>
-
+    <p>
+    </p>
 </body>
 </html>
